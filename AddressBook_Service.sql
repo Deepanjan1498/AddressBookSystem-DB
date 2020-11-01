41e50c125b84d53e27f76519132994d475a239c0
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-11-01 21:40:28 +0530

    [Deepanjan]Add. To use Normalization Technique.

--- AddressBook_Service.sql
+++ AddressBook_Service.sql
@@ -77,3 +77,71 @@ INSERT INTO address_book
 VALUES
 ('Tushar', 'Kumar', 'thirdaddress', 'Dhanbad','Jharkhand', 128457, 9877452222, 'Tusrt52@email.com','Addrress Book 3','Family'),
 ('Uttam', 'Sharma', 'Bankmore', 'jamshedpur','Jharkhand', 123457, 9879682252, 'uttam54@email.com','Addrress Book 4','Friends');
+
+#UC12
+CREATE TABLE user_details
+( 
+ user_id     INT            NOT NULL,
+ first_name  VARCHAR(150)   NOT NULL,
+ last_name   VARCHAR(150)   NOT NULL,
+ PRIMARY KEY   (user_id)
+);
+
+CREATE TABLE location 
+(
+ user_id     INT    NOT NULL,
+ address     VARCHAR(250) NOT NULL,
+ city        VARCHAR(150) NOT NULL,
+ state       VARCHAR(150)  NOT NULL,
+ zip         INT           NOT NULL,
+ PRIMARY KEY(user_id),
+ FOREIGN KEY(user_id)  REFERENCES user_details(user_id)
+); 
+
+CREATE TABLE Contact
+(
+ user_id INT,
+ phone 	BIGINT,
+ email 	VARCHAR(250),
+ FOREIGN KEY(user_id)  REFERENCES user_details(user_id)
+);
+
+CREATE TABLE Contact_type
+(
+ type_id INT,
+ type_of_contact VARCHAR(150),
+ PRIMARY KEY (type_id)
+);
+
+CREATE TABLE user_contact_type_link
+(
+ user_id INT,
+ type_id INT,
+ FOREIGN KEY(user_id) REFERENCES user_details(user_id),
+ FOREIGN KEY(type_id) REFERENCES Contact_type(type_id)
+);
+INSERT INTO user_details VALUES
+	(1, 'Subham', 'kumar'),
+	(2, 'Abhishek', 'singh'),
+	(3, 'Mrinal', 'parihar');
+    
+     
+INSERT INTO location VALUES
+ 	(1,'avav', 'Bangalore', 'Karnataka', '55856'),
+	(2,'bsbgw', 'Delhi','wgbd', '2892'),
+	(3,'hewrtnv', 'Indore', 'MP', '198995');
+    
+INSERT INTO Contact VALUES 
+	(1,'2588668452', 'baebebd@email.com'),
+	(2,'8965321457', 'aaebe@email.com'),
+	(3,'4569823145', 'snebnhbe@email.com');
+    
+INSERT INTO Contact_type VALUES
+ 	(100,'Friends'),
+ 	(101,'Family');
+    
+INSERT INTO user_contact_type_link VALUES
+ 	(1,100),
+ 	(1,101),
+ 	(2,100),
+ 	(3,101);
