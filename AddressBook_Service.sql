13faa7c1e5653a9c4bc818a9ac96f31c272b5ec4
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-11-01 17:06:53 +0530

    [Deepanjan]Add. To edit existing contacts.

--- AddressBook_Service.sql
+++ AddressBook_Service.sql
@@ -23,4 +23,8 @@ INSERT INTO address_book VALUES
 	( 'Deepanjan', 'Sharma', 'firstaddress', 'BGP', 'Bihar', 128956, 9876896210, 'dsfe34@email.com' ),
 	( 'Adarsh', 'Kumar', 'secondaddress', 'Dhanbad','Jharkhand', 128457, 9877452222, 'Adarrt52@email.com'),
 	( 'Sandeep', 'Singh', 'Thirdaddress', 'Bhopal', 'MP', 256957, 8876312010, 'Sandy34@email.com' );
-    
+  
+  #UC4
+UPDATE address_book 				
+SET zip = 986325 
+WHERE first_name = 'Sandeep';
